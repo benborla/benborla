b7ef38ccd34cd5b50fb71c1025c8844973082dd9
Ben Borla <ben.borla@ballys.com>
2023-08-04 09:19:21 +0800

    lint fix

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -19,15 +19,17 @@ export type Ref = HTMLButtonElement;
 
 const defaultStyle = "bg-lime-400 px-5 py-2 hover:bg-neongreen border-b-gray-400 border-r-gray-500 border-b-3 border-r-3 active:border-l-3 active:border-r-0 active:border-r-gray-500 active:border-b-gray-700";
 
-export const Button = forwardRef<Ref, Props>((props, ref) => (
+const Button = forwardRef<Ref, Props>((props, ref) => (
   props.type === Types.LINK ? (
     <Link href= { props.href || '' } className = { defaultStyle + (props.className || '')}>
       { props.children }
-      < /Link>
+      </Link>
   ) : <button ref= { ref } type = { props.type } className = { defaultStyle + (props.className || '')}>
   { props.children }
-  < /button>
+  </button>
   
 ));
+
+Button.displayName = 'Button'
 
 export default Button;
