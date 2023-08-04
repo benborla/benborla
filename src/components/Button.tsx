39e02dcbedd654b819e90aaa32bd3ce52dd45745
Ben Borla <ben.borla@ballys.com>
2023-08-04 09:45:11 +0800

    disable selection on button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -17,7 +17,7 @@ interface Props {
 
 export type Ref = HTMLButtonElement;
 
-const defaultStyle = "bg-lime-400 px-5 py-2 active:bg-neongreen border-b-gray-400 border-r-gray-500 border-b-3 border-r-3 active:border-l-3 active:border-r-0 active:border-r-gray-500 active:border-b-gray-700";
+const defaultStyle = "bg-lime-400 px-5 py-2 active:bg-neongreen border-b-gray-400 border-r-gray-500 border-b-3 border-r-3 active:border-l-3 active:border-r-0 active:border-r-gray-500 active:border-b-gray-700 select-none";
 
 const Button = forwardRef<Ref, Props>((props, ref) => (
   props.type === Types.LINK ? (
